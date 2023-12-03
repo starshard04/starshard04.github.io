dc767efd11d1f2513b9451426a40cbae381f3b62
starshard04 <152766081+starshard04@users.noreply.github.com>
2023-12-03 20:58:12 +0900

    .

---
+++ node_modules/package-name-regex/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _default = /^(@[a-z0-9-~][a-z0-9-._~]*\/)?[a-z0-9-~][a-z0-9-._~]*$/;
+exports.default = _default;
+module.exports = exports.default;
